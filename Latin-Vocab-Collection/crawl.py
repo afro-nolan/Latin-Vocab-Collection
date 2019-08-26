3d73b618a7806b452b523c6a53b897f6d91490f1
Aifric Nolan <32779966+afro-nolan@users.noreply.github.com>
2019-08-26 16:24:24 +0100

    Starting to implement bs4

--- Latin-Vocab-Collection/crawl.py
+++ Latin-Vocab-Collection/crawl.py
@@ -1,26 +1,35 @@
 import requests
 from general import *
 from bs4 import BeautifulSoup
+import os
 
 def main():
 	homepage = "https://en.wikipedia.org/wiki/List_of_Latin_words_with_English_derivatives"
-	crawled_file = proj_name + '/crawled.txt'
-	create_file(create_file)
-
-
+	crawled_file = 'latin.txt'
+	create_file(crawled_file)
+	gather_words(homepage)
 
 def create_file(crawled):
 	if not os.path.isfile(crawled):
-        write_file(crawled, "")
+		write_file(crawled, "")
 
-def write_file(path, data):
-    with open(path, "w") as f:
-        f.write(data)
+def write_file(file, data):
+	print(file)
+	with open(file, "w") as f:
+		f.write(data)
 
 def append_to_file(path, data):
     with open(path, "a") as f:
         f.write(data + "\n")
 
-def gather_words():
-	pass
+def gather_words(page):
+	source_code = requests.get(page)
+	plain_text = source_code.text
+	soup = BeautifulSoup(plain_text, 'html.parser')
+	table = soup.find_all('tbody')
+	soup1 = table.text
+	soup1 = BeautifulSoup(soup1, 'html.parser')
+	print(soup1.find_all('a'))
 
+if __name__ == "__main__":
+	main()
